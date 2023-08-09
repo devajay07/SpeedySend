ec6cce1eed0d18d9f028fd16aa43c81c51aab0dc
Ajay Yadav <ayyadav179@gmail.com>
2023-08-10 00:35:34 +0530

    resolved one feedback

--- public/utils/handleLabel.js
+++ public/utils/handleLabel.js
@@ -1,8 +1,9 @@
 function handleLabel() {
   if (buddy === "")
-    document.getElementById("label").textContent =
-      "Share the roomId with your buddy!";
-  else
+    document.getElementById("label").textContent = `share it with your buddy`;
+  else {
     document.getElementById("label").textContent =
       "Share any file or send a message";
+    roomIdInfoContainer.style.display = "none";
+  }
 }
